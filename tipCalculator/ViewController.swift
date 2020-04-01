bb8b9eeccfe62892a0a7b980dbb60ec7ac2eb568
Jordan Young <jordanby@uci.edu>
2020-04-01 16:27:09 -0700

    Initial Commit

---
+++ tipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipCalculator
+//
+//  Created by Jordan Young on 4/1/20.
+//  Copyright © 2020 Jordan Young. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
